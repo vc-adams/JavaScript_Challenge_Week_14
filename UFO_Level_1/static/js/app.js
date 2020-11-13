248f50e155727a25d0e00885247db6317e3378da
Change vc-adams <vincent.c.adams@gmail.com>
2020-11-12 23:52:44 -0600

    Upadted the date filter JavaScript Level 1.

--- UFO_Level_1/static/js/app.js
+++ UFO_Level_1/static/js/app.js
@@ -7,23 +7,27 @@ var tableData = data;
 var button = d3.select("#filter-btn");
 
 
-// Complete the click handler for the form
+// The click handler for the form
 button.on("click", function () {
 
     // Select the input element and get the raw HTML node
     var inputElement = d3.select("#datetime");
 
     // Get the value property of the input element
-    var inputValue = inputElement.property("value");
-    console.log(inputValue);
+    var inputValueDate = inputElement.property("value");
+    console.log(inputValueDate);
 
     // Use the form input to filter the data by blood type
     // This is the filter function
-    function dateFilter(sighting) {
-        return sighting.dateFilter = inputValue
+    function dateFilter(tableData) {
+        return tableData.datetime == inputValueDate
     }
-
     var sightingsFilteredList = tableData.filter(dateFilter)
+    //------------------------------------------------------------------------------------------------------
+    // OR, you can use the arrow function to filter
+
+    // var sightingsFilteredList = tableData.filter(sighting => sighting.datetime === inputValueDate)
+    //------------------------------------------------------------------------------------------------------
 
     console.log(sightingsFilteredList)
 
@@ -39,33 +43,37 @@ button.on("click", function () {
     // var variance = math.var(ages);
     // var standardDeviation = math.std(ages);
 
-    // // Then select the table
-    // var summaryTable = d3.select("table>tbody")
+    // Then select the table
+    var summaryTable = d3.select("table>tbody")
 
-    // // Clear the data from the table within the HTML file
-    // summaryTable.html("");
+    // Clear the data from the table within the HTML file
+    summaryTable.html("");
 
 
-    // tableData.forEach(sighting => {
-    //     console.log(sighting)
+    tableData.forEach(sightingsFilteredList => {
+        // console.log(sightingsFilteredList)
 
-    //     // Finally, add the summary stats to the table
-    //     // Append an emtpy row to the table
-    //     // var row = summaryTable.append("tr");
+        // Finally, add the summary stats to the table
+        // Append an emtpy row to the table
+        // var row = summaryTable.append("tr");
 
-    //     // Object.values(sighting).forEach(value => {
+        // Object.values(sightingsFilteredList).forEach(([key, value]) => {
+        //     var cell = row.append("td");
+        //     cell.text(value);
 
-    //     var row = summaryTable.append("tr");
+        // });
 
-    //     // Append stats to the table
-    //     summaryTable.append("td").text(sighting.datetime);
-    //     summaryTable.append("td").text(sighting.city);
-    //     summaryTable.append("td").text(sighting.state);
-    //     summaryTable.append("td").text(sighting.country);
-    //     summaryTable.append("td").text(sighting.shape);
-    //     summaryTable.append("td").text(sighting.durationMinutes);
-    //     summaryTable.append("td").text(sighting.comments);
+        var row = summaryTable.append("tr");
 
-    // })
+        // Append stats to the table
+        summaryTable.append("td").text(sightingsFilteredList.datetime);
+        summaryTable.append("td").text(sightingsFilteredList.city);
+        summaryTable.append("td").text(sightingsFilteredList.state);
+        summaryTable.append("td").text(sightingsFilteredList.country);
+        summaryTable.append("td").text(sightingsFilteredList.shape);
+        summaryTable.append("td").text(sightingsFilteredList.durationMinutes);
+        summaryTable.append("td").text(sightingsFilteredList.comments);
 
+
+    });
 });
